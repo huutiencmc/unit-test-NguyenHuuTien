1befa3a66cbb616bfa4630344e8e66e471ff4510
huutiencmc <huutien012004@gmail.com>
2025-05-21 15:31:08 +0700

    Fixes #1: Hoàn thành xử lý logic cho countExcellentStudents

---
+++ src/main/java/com/example/studentanalyzer1/StudentAnalyzer1Application.java
@@ -0,0 +1,13 @@
+package com.example.studentanalyzer1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAnalyzer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentAnalyzer1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/studentanalyzer1/StudentAnalyzer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentanalyzer1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAnalyzer1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
